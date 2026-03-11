a6a08513f53c53938f896891fd06f56c3833023c
micsamuel <mykelsown@gmail.com>
2026-03-11 15:01:53 +0000

    Was able to handle printing of styled character individually

--- methodsandtests/style_printer.go
+++ methodsandtests/style_printer.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func Printer(str string) (string, string, int, int) {
+func Printer(str string) (string, int, int) {
 	bufferedStyleFile := FileHandler()
 	line := 1
 	chLine := 0
@@ -15,18 +15,18 @@ func Printer(str string) (string, string, int, int) {
 			charStartingLine := make([]int, len(str))
 			if ch >= ' ' || ch <= '~' {
 				intCh := int(ch - ' ')
-				chLine = intCh * 9 + 1
+				chLine = intCh*9 
 				charStartingLine = append(charStartingLine, chLine)
 			}
-		}
-		char.WriteString(bufferedStyleFile.Text() + "\n")
-		if line == 18 {
-			break
+			if line >= int(chLine) && line <= int(chLine)+9 {
+				char.WriteString(bufferedStyleFile.Text() + "\n")
+				break
+			}
 		}
 		line++
 	}
 	if err := bufferedStyleFile.Err(); err != nil {
 		log.Fatal(err)
 	}
-	return str, char.String(), line, chLine
+	return  char.String(), line, chLine
 }
